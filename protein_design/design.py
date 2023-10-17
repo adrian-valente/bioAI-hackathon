23b860eaf64ebf7487e7d9e77b0c48d928d70db2
OliverT1 <oturnbull1@gmail.com>
2023-10-17 10:16:03 +0100

    Working mutatin code

--- protein_design/design.py
+++ protein_design/design.py
@@ -1,1 +1,122 @@
-import 
+import pandas as pd 
+import os
+# from ImmuneBuilder import ABodyBuilder2
+import subprocess
+import anarci
+from pathlib import Path
+
+# from ImmuneBuilder import ABodyBuilder2
+from transformers import AutoModel, AutoTokenizer,EsmForMaskedLM
+import argparse
+import torch
+import mutate
+CDR1 = range(27, 39)
+CDR2 = range(56, 66)
+CDR3 = range(105, 118)
+def anarci_to_dict(anarci_numbering):
+    dict = {k : v for k, v in anarci_numbering}
+    return dict
+      
+
+def define_mutateable(VH_numbering, VL_numbering):
+    paragraph_results = pd.read_csv('paragraph_results.csv')
+    # cut-off of 0.734 for predicting in paratope
+    paratope = paragraph_results[paragraph_results['pred'] > 0.734]
+    paratope_VH_positions = paratope[paratope['chain_type'] == 'H']['IMGT'].values
+    paratope_VL_positions = paratope[paratope['chain_type'] == 'L']['IMGT'].values
+
+    # set all positions not in paratope_VH_positions in VH_numbering to X
+    VH_can_mutate = []
+    # remove the deletions from VH_numbering
+    VH_numbering = {k : v for k, v in VH_numbering.items() if v != '-'}
+    VL_numbering = {k : v for k, v in VL_numbering.items() if v != '-'}
+        
+    for position in VH_numbering.keys():
+        imgt_position = str(position[0]) + position[1]
+    
+        if imgt_position not in paratope_VH_positions and position[0] not in CDR1 and position[0] not in CDR2 and position[0] not in CDR3:
+            VH_can_mutate.append(True)
+        else:
+            VH_can_mutate.append(False)
+            
+    VL_can_mutate = []
+    for position in VL_numbering.keys():
+        imgt_position = str(position[0]) + position[1]
+        
+        if imgt_position not in paratope_VL_positions and position[0] not in CDR1 and position[0] not in CDR2 and position[0] not in CDR3:
+            VL_can_mutate.append(True)
+        else:
+            VL_can_mutate.append(False)
+
+    # create dict of residue to if mutateable
+    #combine dicts
+    sequence = "".join(VH_numbering.values()) +  "".join(VL_numbering.values())
+    mutateable = VH_can_mutate + VL_can_mutate
+    return sequence, mutateable
+
+
+# set args
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    default_VH = 'EVQLVESGGGLVQPGRSLRLSCAASGFTFDDYAMHWVRQAPGKGLEWVSAITWNSGHIDYADSVEGRFTISRDNAKNSLYLQMNSLRAEDTAVYYCAKVSYLSTASSLDYWGQGTLVTVSS'
+    default_VL = 'DIQMTQSPSSLSASVGDRVTITCRASQGIRNYLAWYQQKPGKAPKLLIYAASTLQSGVPSRFSGSGSGTDFTLTISSLQPEDVATYYCQRYNRAPYTFGQGTKVEIK'
+    parser.add_argument('--VH', type=str, default=default_VH)
+    parser.add_argument('--VL', type=str, default=default_VL)
+    parser.add_argument('--n_mutants', type=int, default=5,
+                        help='Number of mutants to generate')
+    parser.add_argument('--model_ab', type=bool, default=True,
+                        help='Whether to model sequence using ABB2. If False, will look for "parent_antibody.pdb" in models/')
+    args = parser.parse_args()
+    VH = args.VH
+    VL = args.VL
+    if args.model_ab:
+        
+        os.makedirs('models', exist_ok=True)
+        pass
+        
+    # make directories
+    VH_numbering = anarci.number(VH)[0]
+    VL_numbering = anarci.number(VL)[0]
+    VH_numbering = anarci_to_dict(VH_numbering)
+    VL_numbering = anarci_to_dict(VL_numbering)
+    
+    import subprocess
+    csv_file = 'parent_antibody,H,L'
+    with open('paragraph_in.csv', 'w') as f:
+        f.write(csv_file)
+    command = 'Paragraph -i models -k paragraph_in.csv -o paragraph_results'
+    # run command with subprocess
+    print("\n#### Predicting paratope residues using Paragraph #### \n")
+    subprocess.run(command, shell=True)
+    
+        
+    sequence, mutateable = define_mutateable(VH_numbering, VL_numbering)    
+    paragraph_results = pd.read_csv('paragraph_results.csv')
+    # cut-off of 0.734 for predicting in paratope
+    paratope = paragraph_results[paragraph_results['pred'] > 0.734]
+    print(f"Identified {len(paratope)} paratope residues... \n")
+    
+    model_name="facebook/esm2_t6_8M_UR50D"
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    model = EsmForMaskedLM.from_pretrained(model_name)
+    model.eval()
+    
+    masked_sequences = []
+    print("#### Randomly masking parent antibody sequence, outside of CDRs and paratope ####")
+    for x in range(args.n_mutants):
+        import copy
+        masked_sequences.append(mutate.get_masking(list(sequence), copy.deepcopy(mutateable), mut_distance=3))
+    
+    print(f"Generated {len(masked_sequences)} masked sequences...")
+    for x in range(3):
+        print(masked_sequences[x])
+    
+    print('\n#### Generating mutants using ESM-2 protein language model ####')
+    mutants = mutate.masked_sequences_to_mutated_sequences(masked_sequences, model, tokenizer)
+    print("Generated mutants, saving results to file...")
+    for x in range(3):
+        print(mutants[x])
+    # save out to file
+    with open('mutants.txt', 'w') as f:
+        for mutant in mutants:
+            f.write(mutant + '\n')
---
+++ protein_design/mutate.py
@@ -0,0 +1,108 @@
+from copy import deepcopy
+import copy
+from itertools import combinations
+from re import M
+import typing
+import torch
+import numpy as np
+import random
+
+def numbering_to_chains(numbering):
+    """
+    Takes the output of anarci.anarci and returns a list of heavy and light chains seperated by a '/'.
+    """
+    sequences = numbering[0]
+    sepereated_sequences = []
+    for sequence in sequences:
+        heavy_light = []
+        for chain in sequence:
+            chain = chain[0]
+            for number in chain:
+                residue = number[1]
+                if residue != '-':
+                    heavy_light.append(residue)
+            heavy_light.append('/')
+        heavy_light = "".join(heavy_light[:-1])
+        sepereated_sequences.append(heavy_light)
+    return sepereated_sequences
+
+def masked_sequences_to_mutated_sequences(masked_sequences, model, tokenizer):
+    """
+    Takes in a sequence with <mask> tokens and replaces them with the most likely tokens. One sequence at a time.
+    """
+
+    mutated_sequences = []
+    # Tokenize the sequence and get the output from the model
+    for sequence in masked_sequences:
+        inputs = tokenizer(sequence, return_tensors="pt")
+        # get the index of masked tokens in the sequence
+        masked_index = torch.where(inputs.input_ids == tokenizer.mask_token_id)[1]
+        # remove 1 from each masked_index to account for the <s> token
+        masked_index = masked_index - 1
+        with torch.no_grad():
+            outputs = model(**inputs)
+        logits = outputs.logits[0,1:-1, :]
+        logits.shape
+        # for each masked token, get the most likely token and replace it in the original sequence
+        # convert the <mask> to a single character
+        mutated_sequence = sequence.replace('<mask>', '?')
+        for index in masked_index:
+            max_token = torch.argmax(logits[index])
+            max_token = tokenizer.convert_ids_to_tokens(max_token.item())
+            mutated_sequence = mutated_sequence[:index] + max_token + mutated_sequence[index+1:]
+        mutated_sequences.append(mutated_sequence)
+    return mutated_sequences
+
+def generate_all_maskings(sequences, mutateable_list, mut_distance=1):
+    """
+    Given a seqeunce in the format of a list of tuples, each tuple (amino_acid : str, mutateable : bool) returns a list of masked sequences. Return all sequences with a mutational
+    distance of mut_distance away from the original sequence. Only mutate those with a True value. 
+    Masking means replacing a residue with '<mask>'.
+    Returns a list of sequences (strings).
+    
+    """
+    masked_sequences = []
+    
+    for sequence in sequences:
+        for i in range(len(mutateable_list)):
+            # check if we want to mutate this residue
+            if mutateable_list[i] == True:
+                # change it to a mask
+                masked_sequence = sequence[:i] + '?' + sequence[i+1:]
+                masked_sequences.append(masked_sequence)
+
+    
+    if mut_distance == 1:
+        # replace ? with <mask>
+        masked_sequences = [sequence.replace('?', '<mask>') for sequence in masked_sequences]
+        print(f"Generated {len(masked_sequences)} with {len(set(masked_sequences))} unique sequences with a mutational distance of {mut_distance}")
+        return list(set(masked_sequences))
+    
+    else:
+        return generate_all_maskings(masked_sequences, mutateable_list, mut_distance=mut_distance-1)
+    
+def get_masking(sequence, mutateable_list, mut_distance=1):
+    """
+    Return a sequence with mut_distance number of <mask> tokens. Only mutate those with a True value.
+    """
+    if mut_distance > 0:
+        indices_of_true = [index for index, value in enumerate(mutateable_list) if value]
+        # chose random index to mutate
+        try:
+            indices_to_mutate = random.choice(indices_of_true)
+        except IndexError:
+            print(indices_of_true)
+            print(mutateable_list)
+            
+        # mask the sequence
+        sequence[indices_to_mutate] = '?'
+        mutateable_list[indices_to_mutate] = False
+        return get_masking(sequence, mutateable_list, mut_distance=mut_distance-1)
+    else:
+        sequence = "".join(sequence)
+        sequence = sequence.replace('?', '<mask>')
+        return sequence
+    
+
+        
+    
